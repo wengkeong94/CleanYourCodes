7ac5c0a57c892ecc208636cb669d8bf42b5bee85
Walter Wong <walter@myboost.com.my>
2019-04-14 22:24:50 +0800

    Initial Commit

---
+++ CleanUserTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanUserTest
+//
+//  Created by Walter Wong on 14/04/2019.
+//  Copyright © 2019 CleanYourCodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
